1d3be6742d73c12d001149e2f6f7bc3afd155646
Nicola Anghileri <nicola.anghileri@samtrevano.ch>
2022-03-19 17:54:28 +0100

    db

---
+++ pythonProject/db.py
@@ -0,0 +1,45 @@
+__author__ = "nicolaanghileri"
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    username = db.Column(db.String,primary_key=True)
+    password = db.column(db.Password)
+    children = db.relationship('Child', backref='users', lazy='True')
+
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+    def __repr__(self):
+        return f"{self.username}:"
+
+
+class Child(db.Model):
+
+    __tablename__ = 'child'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
+    birth_date = db.Column(db.Date)
+    weight = db.Column(db.Double)
+    height = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __init__(self, name, birth_date, weight, height):
+        self.name = name
+        self.birth_date = birth_date
+        self.weight = weight
+        self.height = height
+
+    def _repr(self):
+        return f"{self.name}:{self.birth_date}:{self.weight}:{self.height}"
+
+
